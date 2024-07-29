32267841301d2babf56a8f5c3364d554c9c527ef
Francis Veer <2424020953@qq.com>
2024-07-29 18:38:25 +0800

    build

---
+++ SQL-Truck/SQL-Truck/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SQL_Truck.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
